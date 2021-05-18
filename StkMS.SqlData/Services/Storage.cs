70636e7bbb7dcf05087a9c07640858ccf4bdf109
Marcel Popescu <mdpopescu@gmail.com>
2021-05-18 11:51:44 +0300

    Added the timer to the index page and made the storage thread-safe

--- StkMS.SqlData/Services/Storage.cs
+++ StkMS.SqlData/Services/Storage.cs
@@ -7,22 +7,40 @@ namespace StkMS.SqlData.Services
     public class Storage<T> : IStorage<T>
     {
         /// <inheritdoc />
-        public IEnumerable<T> GetAll() => dict.Values.AsEnumerable();
+        public IEnumerable<T> GetAll()
+        {
+            lock (GATE)
+            {
+                return dict.Values.AsEnumerable();
+            }
+        }
 
         /// <inheritdoc />
         public T? this[string key]
         {
-            get => dict.ContainsKey(key) ? dict[key] : default;
+            get
+            {
+                lock (GATE)
+                {
+                    return dict.ContainsKey(key) ? dict[key] : default;
+                }
+            }
             set
             {
-                if (value is { })
-                    dict[key] = value;
-                else
-                    dict.Remove(key);
+                lock (GATE)
+                {
+                    if (value is { })
+                        dict[key] = value;
+                    else
+                        dict.Remove(key);
+                }
             }
         }
 
         //
+
+        // ReSharper disable once StaticMemberInGenericType
+        private static readonly object GATE = new();
 
         private readonly Dictionary<string, T> dict = new();
     }
