d1318535c6498d664302d8608ba0396a2cd8313f
Paul Petre <paul.petre98@yahoo.com>
2021-06-28 20:35:01 +0300

    Customers component added

---
+++ StkMS/Pages/Customers.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StkMS.Pages
+{
+    public partial class CustomersComponent
+    {
+
+    }
+}
