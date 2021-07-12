0dcb97fd74e763e034bdba4e2ccef5b8e81b81ef
Paul <paul.petre98@yahoo.com>
2021-07-12 11:12:04 +0300

    Resolved font errors and changed localhost to server db

--- StkMS.Library/Constants.cs
+++ StkMS.Library/Constants.cs
@@ -4,7 +4,7 @@
     {
         // https://api-stkms.renfieldsoftware.com
         // "https://localhost:44378";
-        public const string API_BASE_URL = "https://localhost:44378";
+        public const string API_BASE_URL = "https://api-stkms.renfieldsoftware.com";
 
         public const string INVALID_PRODUCT_CODE = "INVALID PRODUCT CODE";
     }
