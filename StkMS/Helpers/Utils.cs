769a59d81da8b76d77aa000cc96d54cf83e3352f
Paul <paul.petre98@yahoo.com>
2021-07-16 09:33:15 +0300

    Authorized attributes disabled, culture DateTime changed for Romanian

---
+++ StkMS/Helpers/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StkMS.Helpers
+{
+    public static class Utils
+    {
+        public static DateTime? ParseDateTimeWithCulture(this string s)
+        {
+            s ??= "";
+
+            var cultures = new[]
+            {
+                new CultureInfo("ro-RO"),
+            };
+
+            return cultures
+                .Select(c => DateTime.TryParse(s, c, DateTimeStyles.None, out var result) ? (DateTime?)result : null)
+                .FirstOrDefault(it => it != null);
+        }
+    }
+}
--- StkMS/ViewModels/SaleDetailsViewModel.cs
+++ StkMS/ViewModels/SaleDetailsViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
+using StkMS.Helpers;
 
 #nullable disable
 
@@ -11,5 +13,8 @@ namespace StkMS.ViewModels
         public DateTime DateTime { get; set; }
         public SaleViewModel[] Items { get; set; }
         public string TotalValue => Items.Select(it => it.Value).Sum().ToString("F2");
+
+        public DateTime? FormatDateTime => DateTime.Now.ToString().ParseDateTimeWithCulture();
+
     }
 }
